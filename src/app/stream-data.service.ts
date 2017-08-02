d6724a686550c177f04797dd9e0e948f8a32008f
Peter Elbaum <elbaumpj@gmail.com>
2017-08-02 08:41:11 -0400

    cleaning up code/thinking through making multiple requests

--- src/app/stream-data.service.ts
+++ src/app/stream-data.service.ts
@@ -10,7 +10,8 @@ export class StreamDataService {
   constructor(private http: Http) { }
   getStreamData(site?: string): Observable<any[]> {
     return this.http
-               .get('http://services.arcgis.com/v400IkDOw1ad7Yad/ArcGIS/rest/services/USGS_Stream_Gauge_History_Read_Only/FeatureServer/0/query?where=siteName%3D%27' + site + '+NC%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=siteName%2CsiteID%2CdateTime%2Cvalue&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=dateTime&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&sqlFormat=none&f=pjson&token=')
+               .get('http://services.arcgis.com/v400IkDOw1ad7Yad/ArcGIS/rest/services/USGS_Stream_Gauge_History_Read_Only/FeatureServer/0/query?where=siteName%3D%27' + site + '+NC%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=siteName%2CsiteID%2CdateTime%2Cvalue&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=dateTime+desc&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=15&returnZ=false&returnM=false&quantizationParameters=&sqlFormat=none&f=pjson&token=',
+)
                .map((res:Response) => res.json());
   }
 
